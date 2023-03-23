8dc85df30763dc4e0cd564ddeb3e7e91b1082042
cp6 <cp6@users.noreply.github.com>
2023-03-23 23:28:42 +1100

    Updated dark/light mode switch

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -6,11 +6,12 @@ import Dropdown from "@/Components/Dropdown";
 export default function NavBar({auth, type = 'submit', className = '', processing, children, onClick}) {
 
     const user = auth;
-    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-    function processThemeChange(themeToggleDarkIcon, themeToggleLightIcon) {
-        themeToggleDarkIcon.classList.toggle('hidden');
-        themeToggleLightIcon.classList.toggle('hidden');
 
+    const [isDarkTheme, setIsDarkTheme] = useState(document.documentElement.classList.contains('dark'));
+
+    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+
+    function processThemeChange() {
         if (localStorage.getItem('color-theme')) {
             if (localStorage.getItem('color-theme') === 'light') {
                 document.documentElement.classList.add('dark');
@@ -31,20 +32,8 @@ export default function NavBar({auth, type = 'submit', className = '', processin
     }
 
     function switchTheme() {
-        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            themeToggleLightIcon.classList.remove('hidden');
-        } else {
-            themeToggleDarkIcon.classList.remove('hidden');
-        }
-
-        var themeToggleBtn = document.getElementById('theme-toggle');
-
-        themeToggleBtn.addEventListener('click', function () {
-            processThemeChange(themeToggleDarkIcon, themeToggleLightIcon);
-        });
+        setIsDarkTheme(isDarkTheme => !isDarkTheme);
+        processThemeChange();
     }
 
     return (
@@ -64,6 +53,25 @@ export default function NavBar({auth, type = 'submit', className = '', processin
                             </NavLink>
                             <NavLink href={route('lense.index')} active={route().current('lense.*')}>
                                 Lenses
+                            </NavLink>
+                            <NavLink>
+                                <button id="theme-toggle" onClick={switchTheme} type="button"
+                                        className="text-gray-500 dark:text-gray-400 rounded-md text-sm p-2.5 themeToggle">
+                                    <svg id="theme-toggle-dark-icon"
+                                         className={(isDarkTheme) ? "w-5 h-5 hidden" : "w-5 h-5 text-gray-600 hover:text-gray-900"} fill="currentColor"
+                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                                    </svg>
+                                    <svg id="theme-toggle-light-icon"
+                                         className={(isDarkTheme) ? "w-5 h-5 text-gray-200 hover:text-gray-100" : "w-5 h-5 hidden"}
+                                         fill="currentColor"
+                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                            fillRule="evenodd" clipRule="evenodd"></path>
+                                    </svg>
+                                </button>
                             </NavLink>
                             {
                                 (() => {
@@ -123,20 +131,6 @@ export default function NavBar({auth, type = 'submit', className = '', processin
                             }
                         })()
                     }
-                    <button id="theme-toggle" onClick={switchTheme} type="button"
-                            className="text-gray-900 hover:text-gray-700 hover:dark:text-gray-200 focus:outline-none rounded-lg text-sm p-1 themeToggle">
-                        <svg id="theme-toggle-dark-icon" className="w-5 h-5 hidden" fill="currentColor"
-                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                        </svg>
-                        <svg id="theme-toggle-light-icon" className="w-5 h-5 dark:text-white" fill="currentColor"
-                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                fillRule="evenodd" clipRule="evenodd"></path>
-                        </svg>
-                    </button>
                     <div className="mr-2 flex items-center sm:hidden">
                         <button
                             onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
@@ -182,7 +176,22 @@ export default function NavBar({auth, type = 'submit', className = '', processin
                                         </div>
                                         <div className="font-medium text-sm text-gray-500">{auth.user.email}</div>
                                     </div>
-
+                                    <button id="theme-toggle-mobile" onClick={switchTheme} type="button"
+                                            className="text-gray-500 dark:text-gray-400 rounded-md text-sm p-2.5 themeToggle">
+                                        <svg id="theme-toggle-dark-icon" className={(isDarkTheme) ? "w-5 h-5 hidden" : "w-5 h-5 text-gray-600 hover:text-gray-900"}
+                                             fill="currentColor"
+                                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                                        </svg>
+                                        <svg id="theme-toggle-light-icon" className={(isDarkTheme) ? "w-5 h-5 text-gray-200 hover:text-gray-100" : "w-5 h-5 hidden"}
+                                             fill="currentColor"
+                                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                                fillRule="evenodd" clipRule="evenodd"></path>
+                                        </svg>
+                                    </button>
                                     <div className="mt-3 space-y-1">
                                         <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
                                         <ResponsiveNavLink method="post" href={route('logout')} as="button">
