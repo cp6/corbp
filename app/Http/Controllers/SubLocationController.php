ca8a0426794046dd631caec5718efa5600cca788
cp6 <cp6@users.noreply.github.com>
2023-04-05 23:56:33 +1000

    Cleaned up Exif and SubLocation controller (removed comments)

--- app/Http/Controllers/SubLocationController.php
+++ app/Http/Controllers/SubLocationController.php
@@ -7,57 +7,36 @@ use Illuminate\Http\Request;
 
 class SubLocationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(SubLocation $subLocation)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(SubLocation $subLocation)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, SubLocation $subLocation)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(SubLocation $subLocation)
     {
         //
