ea15702347cfcdcd1eba9d99d719cfb87aedb774
cp6 <cp6@users.noreply.github.com>
2023-04-06 16:01:29 +1000

    Created response alert component

---
+++ resources/js/Components/ResponseAlert.jsx
@@ -0,0 +1,20 @@
+export default function ResponseAlert({type, text}) {
+    if (type === 'success') {
+        return (
+            <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                 role="alert">
+                {text}
+            </div>
+        );
+    } else if (type === 'failure') {
+        return (
+            <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+                 role="alert">
+                {text}
+            </div>
+        );
+    } else {
+        return (<div className={'py-6'}></div>);
+    }
+
+}
