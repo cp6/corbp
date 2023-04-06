fbd126727769aac2286e5cfbb97a84b98c11f064
cp6 <cp6@users.noreply.github.com>
2023-04-06 23:28:33 +1000

    Added warning to responsive alert component

--- resources/js/Components/ResponseAlert.jsx
+++ resources/js/Components/ResponseAlert.jsx
@@ -13,6 +13,14 @@ export default function ResponseAlert({type, text}) {
                 {text}
             </div>
         );
+    } else if (type === 'warning') {
+        return (
+            <div
+                className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+                role="alert">
+                {text}
+            </div>
+        );
     } else {
         return (<div className={'py-6'}></div>);
     }
