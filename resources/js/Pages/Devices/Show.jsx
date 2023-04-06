5348dbbafa91f3745de115e380a81cdd62af646c
cp6 <cp6@users.noreply.github.com>
2023-04-06 16:02:15 +1000

    Added edit button to show pages

--- resources/js/Pages/Devices/Show.jsx
+++ resources/js/Pages/Devices/Show.jsx
@@ -1,9 +1,18 @@
 import MainLayout from "@/Layouts/MainLayout";
+import CreateButton from "@/Components/CreateButton";
 
 export default function Show(props) {
     return (
-        <MainLayout auth={props.auth} title={props.device.name} header={props.device.name}>
-
+        <MainLayout auth={props.auth} title={props.device.name} header={props.device.name} response={props.response}>
+            <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
+                {
+                    (() => {
+                        if (props.auth.user !== null) {
+                            return (<CreateButton text={'Edit device'} link={route('device.edit', props.device.slug)}/>);
+                        }
+                    })()
+                }
+            </div>
         </MainLayout>
     );
 }
--- resources/js/Pages/Location/Show.jsx
+++ resources/js/Pages/Location/Show.jsx
@@ -1,9 +1,18 @@
 import MainLayout from "@/Layouts/MainLayout";
+import CreateButton from "@/Components/CreateButton";
 
 export default function Show(props) {
     return (
         <MainLayout auth={props.auth} title={props.location.name} header={props.location.name}>
-
+            <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
+                {
+                    (() => {
+                        if (props.auth.user !== null) {
+                            return (<CreateButton text={'Edit location'} link={route('locations.edit', props.location.slug)}/>);
+                        }
+                    })()
+                }
+            </div>
         </MainLayout>
     );
 }
