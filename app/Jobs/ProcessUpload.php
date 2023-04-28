7170cc31a7a649dfa739afe2bc775859ee58ddd9
cp6 <cp6@users.noreply.github.com>
2023-04-28 23:51:36 +1000

    Updated ProcessUpload Job for moving files into public storage

--- app/Jobs/ProcessUpload.php
+++ app/Jobs/ProcessUpload.php
@@ -28,15 +28,21 @@ class ProcessUpload implements ShouldQueue
 
         $file = Storage::disk('private')->get("process/{$this->media->id}.{$this->media->extension}");
 
-        //Make smaller version of the full sized uploaded image
-        Media::createSmallerImage($file, storage_path('app/private/process/') . $this->media->id . '_THUMB.' . $this->media->extension, (int)(0.03 * $this->media->width), (int)(0.03 * $this->media->height));
-        Media::createSmallerImage($file, storage_path('app/private/process/') . $this->media->id . '_SMALL.' . $this->media->extension, (int)(0.09 * $this->media->width), (int)(0.09 * $this->media->height));
-        Media::createSmallerImage($file, storage_path('app/private/process/') . $this->media->id . '_MEDIUM.' . $this->media->extension, (int)(0.2 * $this->media->width), (int)(0.2 * $this->media->height));
-        Media::createSmallerImage($file, storage_path('app/private/process/') . $this->media->id . '.' . $this->media->extension, (int)(0.4 * $this->media->width), (int)(0.4 * $this->media->height));
+        //Make smaller version of the full sized uploaded image and save into public uploads
+        $upload_directory = "app/public/uploads/{$this->media->location->dir}/";
+        $extension = $this->media->extension;
+
+        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_THUMB.' . $extension, (int)(0.03 * $this->media->width), (int)(0.03 * $this->media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_SMALL.' . $extension, (int)(0.09 * $this->media->width), (int)(0.09 * $this->media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_MEDIUM.' . $extension, (int)(0.2 * $this->media->width), (int)(0.2 * $this->media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, (int)(0.4 * $this->media->width), (int)(0.4 * $this->media->height));
 
         //Watermark large and medium version images only
-        Media::watermarkImage($file, storage_path('app/private/process/') . $this->media->id . '.' . $this->media->extension, 120, 100, 'corbpie_watermark_large.png');
-        Media::watermarkImage(Storage::disk('private')->get("process/{$this->media->id}_MEDIUM.{$this->media->extension}"), storage_path('app/private/process/') . $this->media->id . '_MEDIUM.' . $this->media->extension, 80, 80, 'corbpie_watermark_medium.png');
+        Media::watermarkImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, 120, 100, 'corbpie_watermark_large.png');
+        Media::watermarkImage(Storage::disk('public')->get("uploads/{$this->media->location->dir}/{$this->media->id}_MEDIUM.{$extension}"), storage_path($upload_directory) . $this->media->id . '_MEDIUM.' . $extension, 80, 80, 'corbpie_watermark_medium.png');
+
+        //Delete the original uploaded file
+        Storage::disk('private')->delete("process/{$this->media->id}.{$extension}");
     }
 
 }
