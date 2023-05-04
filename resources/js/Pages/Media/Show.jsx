76bd61071d52e7e5453bc40c795c554ca5060f28
cp6 <cp6@users.noreply.github.com>
2023-05-04 15:55:57 +1000

    Updated Media/Show to use the MainLayout with media=true

--- resources/js/Pages/Media/Show.jsx
+++ resources/js/Pages/Media/Show.jsx
@@ -1,10 +1,21 @@
 import MainLayout from "@/Layouts/MainLayout";
+import ImageCard from "@/Components/ImageCard";
 
 export default function Show(props) {
-    return (
-        <MainLayout auth={props.auth} title={props.media.title} header={props.media.title}>
-            <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
 
+    const media = props.media;
+
+    return (
+        <MainLayout auth={props.auth} title={media.title_desc.title} header={media.title_desc.title} media={true}>
+            <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
+                <div className={'grid grid-cols-12'}>
+                    <div className={'col-span-12'}>
+                        <ImageCard>
+                            <a href={props.full_link}><img className={'w-full rounded-md'} src={props.medium_link}
+                                                           title={media.title_desc.title} alt={media.title_desc.title}/></a>
+                        </ImageCard>
+                    </div>
+                </div>
             </div>
         </MainLayout>
     );
