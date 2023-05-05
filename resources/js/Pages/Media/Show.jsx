27e0ad224d68875f143aa04a371e6d3a58a481cc
cp6 <cp6@users.noreply.github.com>
2023-05-05 23:57:49 +1000

    Grid in image card Added taken at datetime below image

--- resources/js/Pages/Media/Show.jsx
+++ resources/js/Pages/Media/Show.jsx
@@ -8,14 +8,23 @@ export default function Show(props) {
     return (
         <MainLayout auth={props.auth} title={media.title_desc.title} header={media.title_desc.title} media={true}>
             <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
-                <div className={'grid grid-cols-12'}>
-                    <div className={'col-span-12'}>
-                        <ImageCard>
+                <ImageCard>
+                    <div className={'grid grid-cols-12'}>
+                        <div className={'col-span-12'}>
                             <a href={props.full_link}><img className={'w-full rounded-md'} src={props.medium_link}
                                                            title={media.title_desc.title} alt={media.title_desc.title}/></a>
-                        </ImageCard>
+                        </div>
+                        <div className={'col-span-12 md:col-span-4 text-center'}>
+                            <p className={'text-gray-700 dark:text-gray-300'}></p>
+                        </div>
+                        <div className={'col-span-12 md:col-span-4 text-center'}>
+                            <p className={'text-gray-700 dark:text-gray-300'}></p>
+                        </div>
+                        <div className={'col-span-12 md:col-span-4 text-center'}>
+                            <p className={'text-gray-700 dark:text-gray-300'}>{media.exif.captured_at}</p>
+                        </div>
                     </div>
-                </div>
+                </ImageCard>
             </div>
         </MainLayout>
     );
