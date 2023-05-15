fb2374d30424d6503ba5c1e91a8ba6f9143695fa
cp6 <cp6@users.noreply.github.com>
2023-05-15 10:59:15 +1000

    Updated ProcessUpload.php to shorten $media accessing

--- app/Jobs/ProcessUpload.php
+++ app/Jobs/ProcessUpload.php
@@ -23,36 +23,38 @@ class ProcessUpload implements ShouldQueue
 
     public function handle(): void
     {
-        Log::debug("ProcessUpload STARTED: {$this->media->id} {$this->media->original_filename} {$this->media->width}x{$this->media->height} " . number_format($this->media->height / 1024, 2) . "MB");
+        $media = $this->media;
 
-        $file = Storage::disk('private')->get("process/{$this->media->id}.{$this->media->extension}");
+        Log::debug("ProcessUpload STARTED: {$media->id} {$media->original_filename} {$media->width}x{$media->height} " . number_format($media->height / 1024, 2) . "MB");
+
+        $file = Storage::disk('private')->get("process/{$media->id}.{$media->extension}");
 
         //Make smaller version of the full sized uploaded image and save into public uploads
-        $directory = $this->media->directory->name;
+        $directory = $media->directory->name;
         $upload_directory = "app/public/{$directory}/";
-        $extension = $this->media->extension;
+        $extension = $media->extension;
 
         if (!Storage::disk('public')->exists($directory)) {
             Storage::disk('public')->makeDirectory($directory);
         }
 
-        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_THUMB.' . $extension, (int)(0.03 * $this->media->width), (int)(0.03 * $this->media->height));
-        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_SMALL.' . $extension, (int)(0.09 * $this->media->width), (int)(0.09 * $this->media->height));
-        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_MEDIUM.' . $extension, (int)(0.2 * $this->media->width), (int)(0.2 * $this->media->height));
-        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, (int)(0.26 * $this->media->width), (int)(0.26 * $this->media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $media->id . '_THUMB.' . $extension, (int)(0.03 * $media->width), (int)(0.03 * $media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $media->id . '_SMALL.' . $extension, (int)(0.09 * $media->width), (int)(0.09 * $media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $media->id . '_MEDIUM.' . $extension, (int)(0.2 * $media->width), (int)(0.2 * $media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $media->id . '.' . $extension, (int)(0.26 * $media->width), (int)(0.26 * $media->height));
 
         //Watermark large and medium version images only
-        Media::watermarkImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, 200, 180, 'corbpie_watermark_large.png');
-        Media::watermarkImage(Storage::disk('public')->get("{$directory}/{$this->media->id}_MEDIUM.{$extension}"), storage_path($upload_directory) . $this->media->id . '_MEDIUM.' . $extension, 80, 80, 'corbpie_watermark_medium.png');
+        Media::watermarkImage($file, storage_path($upload_directory) . $media->id . '.' . $extension, 200, 180, 'corbpie_watermark_large.png');
+        Media::watermarkImage(Storage::disk('public')->get("{$directory}/{$media->id}_MEDIUM.{$extension}"), storage_path($upload_directory) . $media->id . '_MEDIUM.' . $extension, 80, 80, 'corbpie_watermark_medium.png');
 
         //Delete the original uploaded file
-        Storage::disk('private')->delete("process/{$this->media->id}.{$extension}");
+        Storage::disk('private')->delete("process/{$media->id}.{$extension}");
 
-        $this->media->directory->increment('count');//Add 1 to directory count
+        $media->directory->increment('count');//Add 1 to directory count
 
-        $this->media->update(['processed' => 1, 'display' => 1]);//Mark media as processed and displayable
+        $media->update(['processed' => 1, 'display' => 1]);//Mark media as processed and displayable
 
-        Log::debug("ProcessUpload FINISHED: {$this->media->id}");
+        Log::debug("ProcessUpload FINISHED: {$media->id}");
     }
 
 }
