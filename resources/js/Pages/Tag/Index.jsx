669b87cc89b8e3cbb363d153d20f72ca5191ee2d
cp6 <cp6@users.noreply.github.com>
2023-05-15 23:50:23 +1000

    Added tags to Tag/Index

--- resources/js/Pages/Tag/Index.jsx
+++ resources/js/Pages/Tag/Index.jsx
@@ -5,8 +5,13 @@ import CreateButton from "@/Components/CreateButton";
 
 export default function Index(props) {
     const auth = usePage().props.auth;
+
+    const tags = props.tags;
+
+    const tags_amount = tags.length;
+
     return (
-        <MainLayout auth={auth} title={'Tags'} header={'Tags'}>
+        <MainLayout auth={auth} title={'Image tags'} header={'Image tags'}>
             <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
                 {
                     (() => {
@@ -15,9 +20,20 @@ export default function Index(props) {
                         }
                     })()
                 }
-                <div className="grid gap-2 grid-cols-2 sm:grid-cols-4 sm:gap-4">
-                    <p className={'text-gray-800 dark:text-gray-400'}>You have {props.tags.length} tags</p>
-                </div>
+                {
+                    (() => {
+                        if (tags_amount > 0) {
+                            return (<Card>
+                          <span
+                              className={'text-gray-800 dark:text-gray-200 italic hover:text-gray-900 hover:dark:text-gray-100'}>
+                    {tags.map((tag, i) => <a key={tag.id}
+                                             href={route('tag.show', tag.slug)}>{tag.name}{i === tags_amount - 1 ? '.' : ', '}</a>
+                    )}
+                                </span>
+                            </Card>);
+                        }
+                    })()
+                }
             </div>
         </MainLayout>
     );
