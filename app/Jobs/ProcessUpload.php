a7a9ba8604123ab365ebc7bde408332a9c1eb6ab
cp6 <cp6@users.noreply.github.com>
2023-05-15 10:52:18 +1000

    Updated ProcessUpload.php Uses new Directory object Updates Directory count

--- app/Jobs/ProcessUpload.php
+++ app/Jobs/ProcessUpload.php
@@ -29,7 +29,7 @@ class ProcessUpload implements ShouldQueue
         $file = Storage::disk('private')->get("process/{$this->media->id}.{$this->media->extension}");
 
         //Make smaller version of the full sized uploaded image and save into public uploads
-        $directory = $this->media->location->dir ?? 'default';
+        $directory = $this->media->directory->name;
         $upload_directory = "app/public/{$directory}/";
         $extension = $this->media->extension;
 
@@ -40,17 +40,18 @@ class ProcessUpload implements ShouldQueue
         Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_THUMB.' . $extension, (int)(0.03 * $this->media->width), (int)(0.03 * $this->media->height));
         Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_SMALL.' . $extension, (int)(0.09 * $this->media->width), (int)(0.09 * $this->media->height));
         Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '_MEDIUM.' . $extension, (int)(0.2 * $this->media->width), (int)(0.2 * $this->media->height));
-        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, (int)(0.4 * $this->media->width), (int)(0.4 * $this->media->height));
+        Media::createSmallerImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, (int)(0.26 * $this->media->width), (int)(0.26 * $this->media->height));
 
         //Watermark large and medium version images only
-        Media::watermarkImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, 120, 100, 'corbpie_watermark_large.png');
+        Media::watermarkImage($file, storage_path($upload_directory) . $this->media->id . '.' . $extension, 200, 180, 'corbpie_watermark_large.png');
         Media::watermarkImage(Storage::disk('public')->get("{$directory}/{$this->media->id}_MEDIUM.{$extension}"), storage_path($upload_directory) . $this->media->id . '_MEDIUM.' . $extension, 80, 80, 'corbpie_watermark_medium.png');
 
         //Delete the original uploaded file
         Storage::disk('private')->delete("process/{$this->media->id}.{$extension}");
 
-        $this->media->update(['processed' => 1]);
-    }
+        $this->media->directory->increment('count');//Add 1 to directory count
 
+        $this->media->update(['processed' => 1, 'display' => 1]);//Mark media as processed and displayable
+    }
 
 }
