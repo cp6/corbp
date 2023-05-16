5adc0aa71122fc5503338460b7026e158f3b8991
cp6 <cp6@users.noreply.github.com>
2023-05-16 14:49:53 +1000

    Added description and tags to Media/Show page

--- resources/js/Pages/Media/Show.jsx
+++ resources/js/Pages/Media/Show.jsx
@@ -1,12 +1,13 @@
 import MainLayout from "@/Layouts/MainLayout";
 import ImageCard from "@/Components/ImageCard";
 import {format} from "date-fns";
-import {HiCalendar, HiCamera, HiCog, HiGlobe} from "react-icons/hi";
+import {HiCalendar, HiCamera, HiCog, HiExclamationCircle, HiGlobe, HiTag} from "react-icons/hi";
 import {asset} from "@/Helpers";
 
 export default function Show(props) {
 
     const media = props.media;
+    const tags_amount = media.tags.length;
 
     const location_text = (media.location !== null) ? <a href={route('locations.show', media.location.slug)}>{media.location.name}</a> : '';
     const sub_location_text = (media.sub_location !== null) ? <a href={route('locations.show', media.sub_location_id)}>{media.sub_location.name}</a> : '';
@@ -21,19 +22,32 @@ export default function Show(props) {
                                                            title={media.title_desc.title} alt={media.title_desc.title}/></a>
                         </div>
                         <div className={'col-span-12 md:col-span-4 text-start ml-2'}>
-                            <p className={'text-gray-700 dark:text-gray-300'}><HiGlobe className={'h-5 w-5 mr-1 mb-1 inline'}/>{location_text} {sub_location_text}</p>
+                            <p className={'text-gray-700 dark:text-gray-300'}><HiGlobe className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{location_text} {sub_location_text}</p>
                         </div>
                         <div className={'col-span-12 md:col-span-4 text-center'}>
                             <p className={'text-gray-700 dark:text-gray-300'}></p>
                         </div>
                         <div className={'col-span-12 md:col-span-4 md:text-end'}>
-                            <p className={'text-gray-700 dark:text-gray-300 mr-2'}><HiCalendar className={'h-5 w-5 mr-1 mb-1 inline'}/>{format(new Date(media.exif.captured_at), 'do LLLL yyyy hh:mm:ss a ')}</p>
+                            <p className={'text-gray-700 dark:text-gray-300 mr-2'}><HiCalendar className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{format(new Date(media.exif.captured_at), 'do LLLL yyyy hh:mm:ss a ')}</p>
                         </div>
                         <div className={'col-span-12 md:col-span-6 text-start ml-2'}>
-                            <p className={'text-gray-700 dark:text-gray-300'}><HiCamera className={'h-5 w-5 mr-1 mb-1 inline'}/>{media.exif.device.name} with {media.exif.lens.name}</p>
+                            <p className={'text-gray-700 dark:text-gray-300'}><HiCamera className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{media.exif.device.name} with {media.exif.lens.name}</p>
                         </div>
                         <div className={'col-span-12 md:col-span-6 md:text-end'}>
-                            <p className={'text-gray-700 dark:text-gray-300 mr-2'}><HiCog className={'h-5 w-5 mr-1 mb-1 inline'}/>{media.exif.f_stop_raw} {media.exif.focal_length}mm {media.exif.shutter_speed_raw} ISO {media.exif.iso}</p>
+                            <p className={'text-gray-700 dark:text-gray-300 mr-2'}><HiCog className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{media.exif.f_stop_raw} {media.exif.focal_length}mm {media.exif.shutter_speed_raw} ISO {media.exif.iso}</p>
+                        </div>
+                        <div className={'col-span-12'}>
+                            <p className={'text-gray-700 dark:text-gray-300 ml-2'}><HiExclamationCircle className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{media.title_desc.description}</p>
+                        </div>
+                        <div className={'col-span-12'}>
+                            <p className={'text-gray-700 dark:text-gray-300 ml-2'}><HiTag className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>
+                                <span
+                                    className={'text-gray-800 dark:text-gray-200 italic hover:text-gray-900 hover:dark:text-gray-100'}>
+                    {media.tags.map((tag, i) => <a key={tag.id}
+                                             href={route('tag.show', tag.tag.slug)}>{tag.tag.name}{i === tags_amount - 1 ? '.' : ', '}</a>
+                    )}
+                                </span>
+                            </p>
                         </div>
                     </div>
                 </ImageCard>
