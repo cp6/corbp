fc0990a14596e3b3a24de5b67b8a714dfdaa1531
cp6 <cp6@users.noreply.github.com>
2023-05-16 15:44:42 +1000

    Formatting of web.php routes file Removed /test route

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+//Random media
 Route::get('/random', [MediaController::class, 'getRandom'])->name('random');
 
+//Media
+Route::get('/m/{media}', [MediaController::class, 'show'])->name('media.show');
+
+//Stats
 Route::get('/stats', [MediaController::class, 'statsPage'])->name('stats');
 
+//Locations
 Route::get('/locations', [LocationController::class, 'index'])->name('locations.index');
 Route::get('/locations/create', [LocationController::class, 'create'])->middleware(['auth'])->name('locations.create');
 Route::get('/locations/{location:slug}', [LocationController::class, 'show'])->name('locations.show');
 
+//Sub locations
 Route::get('/sub-locations', [SubLocationController::class, 'index'])->name('sub-location.index');
 Route::get('/sub-locations/create', [SubLocationController::class, 'create'])->middleware(['auth'])->name('sub-location.create');
 Route::get('/sub-locations/{subLocation:slug}', [SubLocationController::class, 'show'])->name('sub-location.show');
 
+//Devices
 Route::get('/devices', [DeviceController::class, 'index'])->name('device.index');
 Route::get('/devices/{device:slug}', [DeviceController::class, 'show'])->name('device.show');
 
+//Lenses
 Route::get('/lenses', [LenseController::class, 'index'])->name('lense.index');
 Route::get('/lenses/{lense:slug}', [LenseController::class, 'show'])->name('lense.show');
 
+//Tags
 Route::get('/tags', [TagController::class, 'index'])->name('tag.index');
 Route::get('/tags/create', [TagController::class, 'create'])->middleware(['auth'])->name('tag.create');
 Route::get('/tags/{tag:slug}', [TagController::class, 'show'])->name('tag.show');
 
-Route::get('/m/{media}', [MediaController::class, 'show'])->name('media.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -58,14 +67,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/sub-locations/{subLocation}', [SubLocationController::class, 'destroy'])->name('sub-location.destroy');
 
     //Devices
-    Route::get('/devices/create', [DeviceController::class, 'create'])->name('device.create');
     Route::post('/devices', [DeviceController::class, 'store'])->name('device.store');
     Route::get('/devices/{device:slug}/edit', [DeviceController::class, 'edit'])->name('device.edit');
     Route::patch('/devices/{device}', [DeviceController::class, 'update'])->name('device.update');
     Route::delete('/devices/{device}', [DeviceController::class, 'destroy'])->name('device.destroy');
 
     //Lens
-    Route::get('/lenses/create', [LenseController::class, 'create'])->name('lense.create');
     Route::post('/lenses', [LenseController::class, 'store'])->name('lense.store');
     Route::get('/lenses/{lense:slug}/edit', [LenseController::class, 'edit'])->name('lense.edit');
     Route::patch('/lenses/{lense}', [LenseController::class, 'update'])->name('lense.update');
@@ -81,11 +88,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/upload', [MediaController::class, 'create'])->name('upload');
     Route::get('/upload/progress', [MediaController::class, 'uploadFileProgress'])->name('upload.progress');
     Route::post('/upload', [MediaController::class, 'store'])->name('upload.handle');
-
-});
-
-Route::get('/test', function () {
-    abort(404);
 });
 
 require __DIR__ . '/auth.php';
