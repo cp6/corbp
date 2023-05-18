261c9aa19a7e115f62e1687e1fe0795aa80273f4
cp6 <cp6@users.noreply.github.com>
2023-05-18 11:27:16 +1000

    Deleted unused Home.jsx page file

--- resources/js/Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/Layouts/MainLayout";
-export default function Home({auth}) {
-
-    return (
-        <MainLayout auth={auth} title={'Home'} header={null}>
-
-        </MainLayout>
-    );
-}
