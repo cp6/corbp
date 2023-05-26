b2862f46471b5c6a70aa3cd73bfbdf831ea67299
cp6 <cp6@users.noreply.github.com>
2023-05-27 00:34:43 +1000

    Implemented 'subheader' to Locations Show with the location desc

--- resources/js/Pages/Location/Show.jsx
+++ resources/js/Pages/Location/Show.jsx
@@ -4,28 +4,33 @@ import EditButton from "@/Components/EditButton";
 
 export default function Show(props) {
     return (
-        <MainLayout auth={props.auth} title={props.location.name + ' images'} header={props.location.name}>
+        <MainLayout auth={props.auth} title={props.location.name + ' images'} header={props.location.name} subheader={props.location.desc}>
             <div className="max-w-7xl mx-auto sm:px-4 lg:px-2 space-y-6">
                 {
                     (() => {
                         if (props.auth.user !== null) {
-                            return (<EditButton text={'Edit location'} link={route('locations.edit', props.location.slug)}/>);
+                            return (<EditButton text={'Edit location'}
+                                                link={route('locations.edit', props.location.slug)}/>);
                         }
                     })()
                 }
-                    {
-                        (() => {
-                            if (props.media.length > 0) {
-                                return (<div className="grid gap-2 grid-cols-12 sm:gap-4">{props.media.map(media =>
-                                    <div key={media.id} className={'col-span-6 md:col-span-3'}>
-                                        <a href={route('media.show', media.id)}><img src={asset(media.directory.name, media.id, media.extension, 'SMALL')} alt={media.title_desc.title + ' image'} title={'view ' + media.title_desc.title} className="rounded-md"/></a>
-                                    </div>
-                                    )}</div>);
-                            } else {
-                                return (<p className={'text-gray-600 dark:text-gray-400'}>No images found for this location</p>);
-                            }
-                        })()
-                    }
+                {
+                    (() => {
+                        if (props.media.length > 0) {
+                            return (<div className="grid gap-2 grid-cols-12 sm:gap-4">{props.media.map(media =>
+                                <div key={media.id} className={'col-span-6 md:col-span-3'}>
+                                    <a href={route('media.show', media.id)}><img
+                                        src={asset(media.directory.name, media.id, media.extension, 'SMALL')}
+                                        alt={media.title_desc.title + ' image'} title={'view ' + media.title_desc.title}
+                                        className="rounded-md"/></a>
+                                </div>
+                            )}</div>);
+                        } else {
+                            return (<p className={'text-gray-600 dark:text-gray-400'}>No images found for this
+                                location</p>);
+                        }
+                    })()
+                }
             </div>
         </MainLayout>
     );
