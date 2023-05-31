72220fe80d448843b961c8372a882959393b0dcb
cp6 <cp6@users.noreply.github.com>
2023-05-31 23:21:36 +1000

    Added home link to corbp text Removed unused import for the application logo

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,11 +1,10 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-200 dark:bg-slate-900">
             <div>
-                <Link href="/">
+                <Link href={route('home')}>
                     <h1 className={'text-xl md:text-3xl font-medium text-gray-600 dark:text-gray-300 tracking-wider'}>corbp</h1>
                 </Link>
             </div>
