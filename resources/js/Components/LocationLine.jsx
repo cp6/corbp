30f09a8235efb26225e0b03fc53f267d361adc18
cp6 <cp6@users.noreply.github.com>
2023-06-04 00:29:20 +1000

    Fixed sub location link in the media show page

--- resources/js/Components/LocationLine.jsx
+++ resources/js/Components/LocationLine.jsx
@@ -5,7 +5,7 @@ export default function LocationLine({data}) {
         const location_text = (data.location !== null) ?
             <a href={route('locations.show', data.location.slug)}>{data.location.name}</a> : '';
         const sub_location_text = (data.sub_location !== null) ?
-            <span>, <a href={route('locations.show', data.sub_location_id)}>{data.sub_location.name}</a></span> : null;
+            <span>, <a href={route('sub-location.show', data.sub_location.slug)}>{data.sub_location.name}</a></span> : null;
 
         return (
             <p className={'text-gray-700 dark:text-gray-300'}><HiGlobe
