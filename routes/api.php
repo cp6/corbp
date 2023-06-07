d5b9e9334d72e1efb351cb50ca797e3189bb3b54
cp6 <cp6@users.noreply.github.com>
2023-06-07 23:47:40 +1000

    Move sub locations from location api into auth middleware group

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/sub-locations/{location}', [ApiController::class, 'subLocationsForLocation'])->name('api.sub-location.show');
 
 Route::middleware(['auth:api'])->group(function () {
 
     Route::post('geo', [LocationController::class, 'geoApiCall'])->name('api.geo.query');
+
+    Route::get('/sub-locations/{location}', [ApiController::class, 'subLocationsForLocation'])->name('api.sub-location.show');
 
 });
