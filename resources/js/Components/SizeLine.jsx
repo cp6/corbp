c8583d0a20c2829021457b0cb42c0f62cd53dcb9
cp6 <cp6@users.noreply.github.com>
2023-06-09 23:56:18 +1000

    Changed icon for SizeLine component

--- resources/js/Components/SizeLine.jsx
+++ resources/js/Components/SizeLine.jsx
@@ -1,13 +1,14 @@
 import {numberFormat} from "@/Helpers";
-import {HiCubeTransparent} from "react-icons/hi/index.js";
-
+import {HiOutlineArrowsExpand} from "react-icons/hi/index.js";
 
 export default function SizeLine({size}) {
     {
         return (
             <p className={'text-gray-700 dark:text-gray-300 mr-2'}>
-                <HiCubeTransparent
-                    className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{size.width}<span className='text-sm px-0.5'>x</span>{size.height} {numberFormat(size.size / 1000 / 1000)}<span className='text-sm px-0.5'>MB</span>
+                <HiOutlineArrowsExpand
+                    className={'h-5 w-5 mr-1 mb-1 inline text-gray-400'}/>{size.width}<span
+                className='text-sm px-0.5'>x</span>{size.height} {numberFormat(size.size / 1000 / 1000)}<span
+                className='text-sm px-0.5'>MB</span>
             </p>
         )
     }
