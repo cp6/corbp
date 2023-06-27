0579b805493aa644f1b2c6f474535efcfe4c4c5a
cp6 <cp6@users.noreply.github.com>
2023-06-27 22:29:58 +1000

    Cleanup

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
